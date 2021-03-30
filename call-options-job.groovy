f0a42ad53a59e4545e2adf410f41542680fd90d8
chainsawash <mjkeegan@gmail.com>
2021-03-30 17:48:35 -0400

    Update call-options-job.groovy

--- call-options-job.groovy
+++ call-options-job.groovy
@@ -2,12 +2,13 @@ pipeline {
   agent any
   stages {
     stage('Call Options Job'){
-      def build = build job:
-        'job-with-options',
-        wait: false,
-        parameters: [
-          extendedChoice(name: 'CHANNELS', value: 'dev-team,team-spiders'),
-        ]
+      steps {
+        build job:'job-with-options',
+          wait: false,
+          parameters: [
+            extendedChoice(name: 'CHANNELS', value: 'dev-team,team-spiders'),
+          ]
+      }
     }
   }
 }
